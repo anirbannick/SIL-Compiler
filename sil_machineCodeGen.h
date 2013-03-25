f02e623ad75bf4062b7e0ff6dc7078f3bad8a958
Anirban Das <anirban.nick@gmail.com>
2013-03-25 13:07:31 +0530

    modified sil_machineCodeGen.h

--- sil_machineCodeGen.h
+++ sil_machineCodeGen.h
@@ -1,14 +1,21 @@
 
-
-
-
-
-
-
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int rnum = 0;
-void assemble(struct symbol *temp)
+
+
+void Gassemble(struct Gsymbol *temp)
+{
+    if(temp != NULL)
+    {
+        printf("MOV R%d, [%d]\n", rnum, temp->binding);
+        rnum++;
+    }
+}
+
+
+void Lassemble(struct Lsymbol *temp)
 {
     if(temp != NULL)
     {
@@ -19,9 +26,9 @@ void assemble(struct symbol *temp)
 
 int assembleconst(char constant[])
 {
-    if(!strcmp(constant,"")) return 0;
-    char chk = constant[0];
-    if((int)chk >= 48 && (int)chk <= 57)
+    if(strcmp(constant,"")==0) return 0;
+    char ch = constant[0];
+    if((int)ch >= 48 && (int)ch <= 57)
     {
         printf("MOV R%d, %s\n",rnum, constant);
         rnum++;
@@ -35,79 +42,86 @@ void add(char *s)
     int flag = assembleconst(s);
     if(!flag && strcmp(s,""))
     {
-        struct symbol *l = lookup(s,0,1);
-        if(l!= NULL) assemble(l);
+        struct Lsymbol *l = Llookup(s,funcCount);
+        if(l!= NULL) Lassemble(l);
+       
+        else {
+			struct Gsymbol *l = Glookup(s);
+			if(l!=NULL)  Gassemble(l);
+             }
     }
 
 }
 void machiceCodeGenerator(struct addrcode *root)
 {
-    struct symbol *left=NULL;
-    struct symbol *right=NULL;
-    struct symbol *var = NULL;
-    char templeft[20],tempright[20];
+    struct Lsymbol *ll=NULL, *lr=NULL;
+    struct Gsymbol *gl=NULL,  *gr=NULL;
+    
+    char tl[20],tr[20];
     int flag = 0;
     printf("START\n");
-    if(root!= NULL)
-    {   while(root!=NULL)
-        {   
+    
+   if(root!= NULL)
+    { 
+	     while(root!=NULL)
+         {   
 
-            if(!strcmp(root->left,"")&&!strcmp(root->right,"")&&!strcmp(root->var,""))
+            if( (strcmp(root->left,"")==0) && (strcmp(root->right,"")==0) &&  (strcmp(root->t,"")==0))
             {
                 printf("%s\n",root->op);
             }
-            else if(!strcmp(root->op,">"))
+            else if(strcmp(root->op,">")==0)
             {   add(root->left);
                 add(root->right);
                 printf("GT R%d, R%d\n",rnum-2,rnum-1);
                 rnum--;
             }
-            else if(!strcmp(root->op,"<"))
+            else if(strcmp(root->op,"<")==0)
             {
                 add(root->left);
                 add(root->right);
                 printf("LT R%d, R%d\n",rnum-2,rnum-1);
                 rnum--;
             }
-            else if(!strcmp(root->op,">="))
+            else if(strcmp(root->op,">=")==0)
             {
                 add(root->left);
                 add(root->right);
                 printf("GE R%d, R%d\n",rnum-2,rnum-1);
                 rnum--;
             }
-            else if(!strcmp(root->op,"<="))
+            else if(strcmp(root->op,"<=")==0)
             {
                 add(root->left);
                 add(root->right);
                 printf("LE R%d, R%d\n",rnum-2,rnum-1);
                 rnum--;
             }
-            else if(!strcmp(root->op,"=="))
+            else if(strcmp(root->op,"==")==0)
             {   
                 add(root->left);
                 add(root->right);
                 printf("EQ R%d, R%d\n",rnum-2,rnum-1);
                 rnum--;
             }
-            else if(!strcmp(root->op,"!="))
+            else if(strcmp(root->op,"!=")==0)
             {
                 add(root->left);
                 add(root->right);
                 printf("NE R%d, R%d\n",rnum-2,rnum-1);
                 rnum--;
             }
-            else if(!strcmp(root->op,"not"))
+            else if(strcmp(root->op,"not")==0)
             {
                 add(root->left);
-                printf("JZ R%d, %s\n",rnum-1, root->var);
+                printf("JZ R%d, %s\n",rnum-1, root->t);
             }
-            else if(!strcmp(root->op,"jmp"))
+            else if(strcmp(root->op,"goto")==0)
             {
-                printf("JMP %s\n",root->var);
+                printf("JMP %s\n",root->t);
             }
            
-            else if(!strcmp(root->op,"+"))
+            else if(strcmp(root->op,"+")==0)
             {
 
                 add(root->left);
@@ -115,28 +129,28 @@ void machiceCodeGenerator(struct addrcode *root)
                 printf("ADD R%d, R%d\n", rnum-2, rnum-1);
                 rnum--;            
             }
-            else if(!strcmp(root->op,"*"))
+            else if(strcmp(root->op,"*")==0)
             {
                 add(root->left);
                 add(root->right);
                 printf("MUL R%d, R%d\n", rnum-2, rnum-1);
                 rnum--;
             }    
-            else if(!strcmp(root->op,"/"))
+            else if(strcmp(root->op,"/")==0)
             { 
                 add(root->left);
                 add(root->right);
                 printf("DIV R%d, R%d\n", rnum-2, rnum-1);
                 rnum--;
             }    
-            else if(!strcmp(root->op,"-")&& strcmp(root->right,"")!=0)
+            else if((strcmp(root->op,"-")==0)  && (strcmp(root->right,"")!=0))
             {
                 add(root->left);
                 add(root->right);
                 printf("SUB R%d, R%d\n", rnum-2, rnum-1);
                 rnum--;
             }    
-            else if(!strcmp(root->op,"%%"))
+            else if(strcmp(root->op,"%%")==0)
             {
 
                 add(root->left);
@@ -145,40 +159,83 @@ void machiceCodeGenerator(struct addrcode *root)
                 rnum--;            
             }
 
-            else if(!strcmp(root->op,"-"))
+            else if(strcmp(root->op,"-")==0)
             {
                 add(root->left);
                 printf("MOV R%d, 0\n",rnum);
                 printf("SUB R%d, R%d\n", rnum, rnum-1);
                 printf("MOV R%d, R%d\n",rnum-1,rnum);
             }    
-            else if(!strcmp(root->op,"="))
+            else if(strcmp(root->op,"=")==0)
             {   add(root->left);
                 add(root->right);
                 rnum--;
-                var = lookup(root->var,0,1);
-                if(var != NULL) printf("MOV [%d], R%d\n", var->binding, rnum);
-                else printf("MOV [R%d], R%d\n", rnum, rnum-1);
+                gtemp = Glookup(root->t);
+                if(gtemp != NULL)
+                           printf("MOV [%d], R%d\n", gtemp->binding, rnum);
+                else {
+				           ltemp = Llookup(root->t,funcCount);
+				           if(ltemp !=NULL)
+                                 	printf("MOV [%d], R%d\n", ltemp->binding, rnum);			          
+                           else 
+                                    printf("MOV [R%d], R%d\n", rnum, rnum-1);
+                     }
+                    
                 rnum = 0;
             }
-            else if(!strcmp(root->op,"Value_at"))
-            {   //add(root->left);
-                left = lookup(root->left,0,1);
-                strcpy(tempright, root->right);
-                int chk = (int) tempright[0];
-                if(!strcmp((root->next)->op,"read")||!strcmp((root->next)->op, "write"))                
+            else if(strcmp(root->op,"Array_Value"))
+            {  
+                ll = Llookup(root->left,funcCount);
+				if (ll==NULL) { gl = Glookup(root->left); }
+				
+                strcpy(tr, root->right);
+                int ch = (int) tr[0];
+                if((strcmp((root->next)->op,"read")==0)  ||   (strcmp((root->next)->op, "write")==0))                
                 {
-                    if(chk >= 48 && chk<= 57)
-                    {    printf("MOV R%d, %d\n", rnum, left->binding + atoi(tempright)); rnum++;}
+                    if(ch >= 48 && ch<= 57)
+                    {   if(lL!=NULL)
+	                        printf("MOV R%d, %d\n", rnum, ll->binding + atoi(tr)); 
+	                    else if (gl!=NULL)
+	                          printf("MOV R%d, %d\n", rnum, gl->binding + atoi(tr)); 
+	
+	                    rnum++;
+	                 }
                     else
                     {
-                        right = lookup(root->right, 0 ,1);
-                        if(right != NULL)
-                        {    printf("MOV R%d, %d\n",rnum, left->binding + right->binding); rnum++;}
-                        else
+                        lr = Llookup(root->right,funcCount);
+						if(lr==NULL) { gr = Glookup(root->right);  }
+                        if(lr != NULL)
+                        {   if(ll!=NULL)
+	                               printf("MOV R%d, %d\n",rnum, ll->binding + lr->binding);
+	                        else if(gl!=NULL)
+	                                printf("MOV R%d, %d\n",rnum, gl->binding + lr->binding);
+	                       
+	
+	                        rnum++;
+	                     }
+	
+	                   else if(gr!=NULL)
+	                    {
+		 
+		                     if ( ll!=NULL)
+		                                 printf("MOV R%d, %d\n",rnum, ll->binding + gr->binding);
+		                        else if (gl!=NULL)
+		                                 printf("MOV R%d, %d\n",rnum, gl->binding + gr->binding);
+		
+		                     rnum++
+	                     }
+	
+	                 
+                        
+
+
+                     else
                         {
                             printf("MOV R%d, R%d\n", rnum, rnum-1);
-                            printf("MOV R%d, %d\n",rnum+1,left->binding);
+                            if(ll!=NULL)
+                                 printf("MOV R%d, %d\n",rnum+1,ll->binding);
+                            else if(gl!=NULL)
+                                  printf("MOV R%d, %d\n",rnum+1,ll->binding);
                             printf("ADD R%d, R%d\n", rnum, rnum+1);
                             rnum++;
                         }    
@@ -186,20 +243,48 @@ void machiceCodeGenerator(struct addrcode *root)
                 }
                 else
                 {
-                    if(chk>=48 && chk<=57)
-                    {
-                        printf("MOV R%d, [%d]\n", rnum, left->binding +  atoi(tempright));
+                    if(ch>=48 && ch<=57)
+                    {  if(ll!=NULL)
+                            printf("MOV R%d, [%d]\n", rnum, ll->binding +  atoi(tr));
+                        else if(gl!=NULL)
+                             printf("MOV R%d, [%d]\n", rnum, gl->binding +  atoi(tr));
+                 
+                        
                         rnum++;
                     }
                     else
                     {
-                        right = lookup(root->right, 0 ,1);
-                        if(right != NULL)
-                        {    printf("MOV R%d, [%d]\n",rnum, left->binding + right->binding); rnum++;}
+                        lr = Llookup(root->right, funcCount);
+						if (lr==NULL) gr=Glookup(root->right);
+                        
+                        if(lr != NULL)
+                        {   if(ll!=NULL)
+	                          printf("MOV R%d, [%d]\n",rnum, ll->binding + lr->binding); 
+	                        
+	                        else if(gl!=NULL)
+	                             printf("MOV R%d, [%d]\n",rnum, gl->binding + lr->binding); 
+	
+	                     rnum++;  
+                     	}
+
+                       else if (gr!=NULL)
+					      {   if(ll!=NULL)
+	                          printf("MOV R%d, [%d]\n",rnum, ll->binding + gr->binding); 
+
+	                        else if(gl!=NULL)
+	                             printf("MOV R%d, [%d]\n",rnum, gl->binding + gr->binding); 
+
+	                     rnum++;  
+                     	}                       
+
+
                         else
                         {
                             printf("MOV R%d, R%d\n", rnum, rnum-1);
-                            printf("MOV R%d, %d\n",rnum+1, left->binding);
+                            if(ll!=NULL)
+                                 printf("MOV R%d, %d\n",rnum+1, ll->binding);
+                             else if(gl!=NULL)
+                                  printf("MOV R%d, %d\n",rnum+1, gl->binding);
                             printf("ADD R%d, R%d\n", rnum, rnum+1);
                             printf("MOV R%d, [R%d]\n",rnum-1, rnum);
                             
@@ -208,13 +293,18 @@ void machiceCodeGenerator(struct addrcode *root)
                 }
             }
             
-            else if(!strcmp(root->op,"read"))
+            else if(strcmp(root->op,"read")==0)
             {
-                left = lookup(root->left, 0 , 1);
-                if(left->nodetype == VAR)
+                ll = Llookup(root->left, funcCount);
+                
+				if(ll==NULL)  gl = Glookup(root->left);
+			
+			if(ll!=NULL)	
+              { 
+	             if(ll->nodetype == VAR)
                 {
                     printf("IN R%d\n",rnum);
-                    printf("MOV [%d], R%d\n",left->binding, rnum);
+                    printf("MOV [%d], R%d\n",ll->binding, rnum);
                 }
                 else
                 {
@@ -223,14 +313,42 @@ void machiceCodeGenerator(struct addrcode *root)
                     
                 }   
                 rnum = 0;
-            }
-            else if(!strcmp(root->op,"write"))
+              
+              }
+
+           else if (gl!=NULL)
+              {	
+	          	if(gl->nodetype == VAR)
+                {
+                  printf("IN R%d\n",rnum);
+                  printf("MOV [%d], R%d\n",gl->binding, rnum);
+               }
+              else
+              {
+                  printf("IN R%d\n",rnum);
+                  printf("MOV [R%d], R%d\n", rnum-1, rnum);
+                  
+              }   
+              rnum = 0;
+
+             }
+	               
+          }
+
+             
+           
+            else if(strcmp(root->op,"write")==0)
             {
-                left = lookup(root->left, 0, 1);
-                if(left!=NULL)
-                    if(left->nodetype == VAR)
+                ll = Llookup(root->left, funcCount);
+	  
+		        if(ll==NULL)  gl = Glookup(root->left);
+
+                
+                if(ll !=NULL)
+                   {
+	                   if(ll->nodetype == VAR)
                     {
-                        printf("MOV R%d, [%d]\n",rnum, left->binding);
+                        printf("MOV R%d, [%d]\n",rnum, ll->binding);
                         printf("OUT R%d\n",rnum);
                     }
                     else
@@ -239,20 +357,41 @@ void machiceCodeGenerator(struct addrcode *root)
                         printf("OUT R%d\n",rnum);
                                             
                     }
-                else
+
+                   }
+                else if (gl!=NULL)
+                {
+	                   if(gl->nodetype == VAR)
+                 {
+                     printf("MOV R%d, [%d]\n",rnum, gl->binding);
+                     printf("OUT R%d\n",rnum);
+                 }
+                 else
+                 {
+                     printf("MOV R%d, [R%d]\n",rnum, rnum-1);
+                     printf("OUT R%d\n",rnum);
+                                         
+                 }
+
+                }
+
+         
+               else
                     printf("OUT R%d\n",rnum-1);    
+                
                 rnum = 0;
-            }
-          root = root->next;
-        }
-    }
+                
+           }
+         
+             root = root->next;
+           
+           }
+    
+      }
 
     printf("HALT\n");
 }
 
 
 
-void assemblycode()
-{
-    assembly(tuples);
-}
+
