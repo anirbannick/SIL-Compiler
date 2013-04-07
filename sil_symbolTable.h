9034235c4c207a1130e4102c4a25650e5d51efd4
Anirban Das <anirban.nick@gmail.com>
2013-04-08 01:58:43 +0530

    Added ArgStruct structure and corresponding Arginstall and Arglookup function

--- sil_symbolTable.h
+++ sil_symbolTable.h
@@ -4,7 +4,20 @@
 #include<stdlib.h>
 //#include<malloc.h>
 //using namespace std;
-int baseg= 1;basel=1000;
+int base=0;
+
+struct ArgStruct
+{
+	char *name;
+	int type;
+	int nodetype;
+	int size;
+	int offset;
+	struct ArgStruct *next;
+	
+	} *headA[10],*tailA[10] ;
+
+
 
 
 
@@ -15,17 +28,21 @@ char *name; // Name of the Identifier
 
 int type; // TYPE can be INTEGER or BOOLEAN
 
+int nodetype;
+
 /***The TYPE field must be a TypeStruct if user defined types are allowed***/
 
-//int size; // Size field for arrays
+int size; // Size field for arrays
 
 int binding; // Address of the Identifier in Memory
 
-//ArgStruct *ARGLIST; // Argument List for functions
+ArgStruct *arglist; // Argument List for functions
 
 /***Argstruct must store the name and type of each argument ***/
 
 struct Gsymbol *next; // Pointer to next Symbol Table Entry */
+
+int fc;				//function count
 
 } *headg=NULL, *tailg=NULL;
 
@@ -37,12 +54,78 @@ struct Lsymbol {
 	char *name; // Name of the Identifier
 
 	int type; // TYPE can be INTEGER or BOOLEAN
+	int nodetype;
 	int binding;
 	/***The TYPE field must be a TypeStruct if user defined types are allowed***/
 	
 	struct Lsymbol *next; // Pointer to next Symbol Table Entry */
 
-	} *headl[50], *taill[50];
+} *headl[10], *taill[10];
+
+
+void Arginstall(char *name,int type,int nodetype,int size,int offset,int fc)
+{
+	printf(" \nentered Arginstall block\n");
+    struct  ArgStruct *temp=(struct ArgStruct *)malloc(sizeof(struct ArgStruct));
+
+     temp->name =  name;
+     temp->type=type;
+	temp->nodetype=nodetype;
+	temp->size=size;
+	temp->offset=offset;	
+     temp->next=NULL;
+
+     	if (headA[fc]==NULL)
+        {
+            headA[fc]=temp;
+            tailA[fc]=temp;   
+          
+         }
+
+     else
+        
+         {
+            tailA[fc]->next=temp;
+            tailA[fc]=tailA[fc]->next;
+          }
+	
+}
+
+
+struct ArgStruct Arglookup(char *name,int fc)
+{
+	struct	ArgStruct *temp=NULL;
+    
+   	printf("\nin Arglookup \nlooking for %s , in function no. - %d\n",name,fc); 
+
+  if(headArg[fc]!=NULL)
+    { temp=headArg[fc];
+   	printf("\n headArg[n]!=NULL\n");
+     while (temp!=NULL)
+       
+         {  	printf("found %s\n",temp->name);
+            if( strcmp(temp->name,name)==0)
+                  {
+                      return temp;
+                         
+                   }
+
+            else
+                 { temp=temp->next;
+	              printf("\ntemp->name not equal to name\n");
+	             }
+
+          }
+       }
+
+    else
+	    {   printf("\n headArg[n]==NULL\n returning NULL\n");
+			return NULL;
+			
+		}
+		
+		return NULL;
+}
 
 
 
@@ -90,17 +173,25 @@ struct Gsymbol *Glookup(char *name) // Look up for a global identifier
 
 
 
-void Ginstall(char *name, int type)   // Installation
+
+
+
+void Ginstall(char *name, int type,int nodetype,int size, struct ArgStruct *arglist,int fc)   // Installation
 {   
-	int size= 4 ;
+		printf(" \nentered Ginstall block\n");
     struct  Gsymbol *temp=(struct Gsymbol *)malloc(sizeof(struct Gsymbol));
 
      temp->name =  name;
      temp->type=type;
-	if (type ==1) size = 1; 
-	temp->binding = baseg;
-	baseg= baseg +size;
+	temp->nodetype=nodetype;
+	temp->size=size;
+		printf(" \nCurrent base value = %d\n",base);
+	temp->binding = base;
+		printf(" \ntemp->binding =  %d\n" ,temp->binding);
+	base= base + size;
 	//temp->size=size;
+	temp->arglist=arglist;
+	temp->fc=fc;
      temp->next=NULL;
 
      	if (headg==NULL)
@@ -132,7 +223,7 @@ struct Lsymbol *Llookup(char *name,int n)
 
   if(headl[n]!=NULL)
     { temp=headl[n];
-   	printf("\n headg[n]!=NULL\n");
+   	printf("\n headl[n]!=NULL\n");
      while (temp!=NULL)
        
          {  	printf("found %s\n",temp->name);
@@ -151,7 +242,7 @@ struct Lsymbol *Llookup(char *name,int n)
        }
 
     else
-	    {   printf("\n headg==NULL\n returning NULL\n");
+	    {   printf("\n headl[n]==NULL\n returning NULL\n");
 			return NULL;
 			
 		}
@@ -162,18 +253,22 @@ struct Lsymbol *Llookup(char *name,int n)
 
 
 
-void Linstall(char *name, int type, int n)
+void Linstall(char *name, int type,int nodetype, int n)
 {
-	int size=4;
+		printf(" \nentered Linstall block\n");
+	
    struct	Lsymbol *temp=(struct Lsymbol *)malloc(sizeof(struct Lsymbol));
 
      temp->name = name;
      temp->type=type;
-	temp->binding = basel;
+	temp->nodetype=nodetype;
+    	printf(" \nCurrent base value = %d\n",base);
+	temp->binding = base;
+		printf(" \ntemp->binding =  %d\n" ,temp->binding);
 	
-	if (type==1) size =1;
 	
-	basel= basel + size;
+	
+	base= base + 1;
 	
 	 
 	
@@ -195,6 +290,61 @@ void Linstall(char *name, int type, int n)
 	
 	
 }
+
+
+void GsymbolTraversal()
+{
+	struct	Gsymbol *tempg=NULL;
+	tempg=headg;
+	if(headg==NULL)
+	{
+		printf("\nGsymbol table is empty\n");
+		exit(1);
+	}
+	printf("\nname				type			nodetype		size			binding			formalParameter\n");
+	while(tempg!=NULL)
+	{   
+		if((tempg->arglist)==NULL)
+			printf("%s			%d			%d			%d			%d			NULL\n",tempg->name,tempg->type,tempg->nodetype,tempg->size,tempg->binding);
+	    else	
+			{
+				printf("%s			%d			%d			%d			%d			Given in next line\n",tempg->name,tempg->type,tempg->nodetype,tempg->size,tempg->binding);
+				printf("Formal parameter list : \n");
+				struct ArgStruct *temparg=tempg->arglist;
+				printf("name			type			nodetype			size\n");
+				while(temparg!=NULL)
+				{
+						printf("%s			%d			%d			%d",temparg->name,temparg->type,temparg->nodetype,temparg->size);
+						temparg=temparg->next;
+				}
+			}
+			tempg=tempg->next;
+	}
+	
+	
+}
+
+
+
+void LsymbolTraversal(int n)
+{
+	struct	Lsymbol *templ=NULL;
+	templ=headl[n];
+	if(headl[n]==NULL)
+	{
+		printf("\nLsymbol table[%d] is empty\n",n);
+		exit(1);
+	}
+	printf("\nname				type			nodetype			binding\n");
+	while(templ!=NULL)
+	{
+		printf("%s			%d			%d\n",templ->name,templ->type,templ->nodetype,templ->binding);
+		templ=templ->next;
+	}
+	
+	
+}
+
 
 
    
